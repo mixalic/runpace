e29ee97caa73c5976d4d95181b45d8d5fda4dc4c
Aleksei Orlov <lexa@mixalic.ru>
2019-09-01 17:22:20 +0300

    add input mask, change style to bootstrap card view

--- src/App.vue
+++ src/App.vue
@@ -1,106 +1,91 @@
 <template>
-  <div class="container">
 
-    <h1>Калькулятор темпа</h1>
+  <div class="container-fluid">
 
-    <form>
-      <div class="form-group">
-        <label for="pace">Темп</label>
-        <input
-                type="text"
-                class="form-control"
-                id="pace"
-                :placeholder="calcPace"
-                aria-describedby="paceHelp"
-                v-model="pace"
-        >
-        <small id="paceHelp" class="form-text text-muted">Введите темп в формате МИНУТЫ:СЕКУНДЫ (например 5:40 мин/км)</small>
+    <div class="card mb-4" style="margin-top: 15px; max-width: 30rem">
+      <div class="card-header">
+        Калькулятор темпа
       </div>
+      <div class="card-body">
+        <h5 class="card-title"></h5>
+        <form>
+          <div class="form-group">
+            <label for="pace">Темп</label>
+            <input
+                    class="form-control"
+                    id="pace"
+                    :placeholder="calcPace"
+                    aria-describedby="paceHelp"
+                    v-model="pace"
+                    onfocus="(this.type='time')" onblur="(this.type='text')"
 
-      <div class="form-group">
-        <label for="time">Время</label>
-        <input
-                type="text"
-                class="form-control"
-                id="time"
-                :placeholder="calcTime"
-                aria-describedby="timeHelp"
-                v-model="time"
-        >
-        <small id="timeHelp" class="form-text text-muted">Введите предпологаемое время забега в формате ЧАСЫ:МИНУТЫ:СЕКУНДЫ (например 00:01:35)</small>
+            >
+            <small id="paceHelp" class="form-text text-muted">Введите темп в формате МИНУТЫ:СЕКУНДЫ (например 05:40 мин/км)</small>
+          </div>
+
+          <div class="form-group">
+            <label for="time">Время</label>
+            <input
+                    type="time"
+                    step="1"
+                    class="form-control"
+                    id="time"
+                    :placeholder="calcTime"
+                    aria-describedby="timeHelp"
+                    v-model="time"
+                    onfocus="(this.type='time')" onblur="(this.type='text')"
+
+            >
+            <small id="timeHelp" class="form-text text-muted">Введите предпологаемое время забега в формате ЧАСЫ:МИНУТЫ:СЕКУНДЫ (например 02:20:42)</small>
+          </div>
+
+          <div class="form-group">
+            <label for="distance">Дистанция</label>
+            <input
+                    type="number"
+                    class="form-control"
+                    id="distance"
+                    :placeholder="calcDistance"
+                    aria-describedby="distanceHelp"
+                    v-model="distance"
+            >
+            <small id="distanceHelp" class="form-text text-muted">Введите или выберите дистанцию забега (например 21.1 или 5 км)</small>
+          </div>
+        </form>
       </div>
-
-      <div class="form-group">
-        <label for="distance">Дистанция</label>
-        <input
-                type="text"
-                class="form-control"
-                id="distance"
-                :placeholder="calcDistance"
-                aria-describedby="distanceHelp"
-                v-model="distance"
-        >
-        <small id="distanceHelp" class="form-text text-muted">Введите или выберите дистанцию забега (например 10 км)</small>
-      </div>
-    </form>
-
-    <!--<div>
-      <Select2
-              theme="bootstrap4"
-              v-model="distance"
-              :options="myOptions"
-              :settings="{ placeholder: '0 км', tags: true }"
-              @change="myChangeEvent($event)"
-              @select="mySelectEvent($event)"
-      />
-    </div>-->
-
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  //components: {Select2},
   data(){
     return {
       pace: '',
       time: '',
       distance: '',
-      /*selectDistance: '',
-      //myOptions: ['Марафон - 42.2 км', 'Полумарафон - 21.1 км', '15 км', '10 км', '5 км', '3 км', '1 км']
-      myOptions: [
-              {id: 42.2, text: '42.2 км (Марафон)'},
-              {id: 21.1, text: '21.1 км (Полумарафон)'},
-              {id: 15, text: '15 км'},
-              {id: 10, text: '10 км'},
-              {id: 5, text: '5 км'},
-              {id: 3, text: '3 км'},
-              {id: 1, text: '1 км'},
-      ]*/
     }
   },
-  /*methods: {
-    myChangeEvent(val){
-      console.log(val);
-    },
-    mySelectEvent({id, text}){
-      console.log({id, text})
-    }
-  },*/
   computed: {
     calcPace(){
-
       if (this.distance == '' || this.time == '') {
         return '00:00/км';
       } else {
 
         let timeArray = [0, 0, 0],
                 timeMinutes = 0,
-                minutes = '',
-                seconds = '';
+                minutes = 0,
+                seconds = 0;
 
         timeArray = this.time.split(":");
-        timeMinutes = +timeArray[0] * 60 + +timeArray[1] + timeArray[2] / 60;
+
+        console.log(timeArray);
+
+        if(timeArray[2])
+          timeMinutes = +timeArray[0] * 60 + +timeArray[1] + timeArray[2] / 60;
+        else
+          timeMinutes = +timeArray[0] * 60 + +timeArray[1];
+
 
         let distance = this.distance;
         let pace = timeMinutes / distance * 60;
@@ -113,7 +98,6 @@ export default {
     },
 
     calcTime() {
-
       if (this.distance == '' || this.pace == '') {
         return '00:00:00';
       } else {
@@ -130,7 +114,7 @@ export default {
 
         timeSeconds = paceSeconds * distance;
 
-        console.log(paceSeconds, '*', distance, '=',timeSeconds,  )
+        //console.log(paceSeconds, '*', distance, '=',timeSeconds,  )
 
         hours = Math.floor(timeSeconds / 3600);
         minutes = Math.floor((timeSeconds % 3600) / 60);
@@ -141,7 +125,6 @@ export default {
     },
 
     calcDistance(){
-
       if(this.time == '' || this.pace == '') {
         return '0 км';
       } else {
--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 import Vue from 'vue'
-import Select2 from 'v-select2-component'
 import App from './App.vue'
+
+import VueMask from 'v-mask'
+Vue.use(VueMask);
+
+import Select2 from 'v-select2-component'
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 Vue.use(Select2);
 Vue.component('Select2', Select2);
@@ -10,3 +18,8 @@ Vue.config.productionTip = false
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+
+library.add(faUserSecret)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
